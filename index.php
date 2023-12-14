0a4a264916b4fb2ebb9818cd6dde511b03ba4e8b
mayerthomas2023 <133607010+mayerthomas2023@users.noreply.github.com>
2023-12-13 23:00:48 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,7 @@
 <?php include_once("home.html"); ?>
+$connString = "host=$ec2-3-232-218-211.compute-1.amazonaws.com port=$5432 dbname=$d7044m9719erbv user=$dgwwsjmrrjnwfm password=$5b51472a52ea04594728c9c42b19ab4ed575f2778cdf20ab4a21884c9352cbae";
+$conn = pg_connect($connString);
+
+if (!$conn) {
+    die("Error in connection: " . pg_last_error());
+}
