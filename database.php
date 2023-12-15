5e1b070d9957b9a8b1d06fd49455dc732be49dfd
mayerthomas2023 <133607010+mayerthomas2023@users.noreply.github.com>
2023-12-14 20:48:10 -0600

    Add files via upload

---
+++ database.php
@@ -0,0 +1,12 @@
+$pdo = new PDO("pgsql:host=ec2-34-236-100-103.compute-1.amazonaws.com;dbname=df7jav21pe862", "igdfndgbifihqx", "4e8923df29872b86da2cd47295c7ac3b9b2b1b9a8d61029e6c47ad0eb792f7c3");
+$stmt = $pdo->prepare("INSERT INTO recipe_submissions (recipe_name, ingredients, steps, additional_notes) VALUES (:recipe_name, :ingredients, :steps, :additional_notes)");
+$stmt->execute([
+    'recipe_name' => $recipeName,
+    'ingredients' => $ingredients,
+    'steps' => $steps,
+    'additional_notes' => $additionalNotes
+]);
+$stmt = $pdo->query("SELECT * FROM recipe_submissions");
+while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+    // process each row
+}
