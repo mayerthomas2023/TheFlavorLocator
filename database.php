12e201e321e8da1d91c3f47b7665cc655b3fe6d8
mayerthomas2023 <133607010+mayerthomas2023@users.noreply.github.com>
2023-12-14 21:40:48 -0600

    Update database.php

--- database.php
+++ database.php
@@ -1,4 +1,4 @@
-$pdo = new PDO("pgsql:host=ec2-34-236-100-103.compute-1.amazonaws.com;dbname=df7jav21pe862", "igdfndgbifihqx", "4e8923df29872b86da2cd47295c7ac3b9b2b1b9a8d61029e6c47ad0eb792f7c3");
+<?php $pdo = new PDO("pgsql:host=ec2-34-236-100-103.compute-1.amazonaws.com;dbname=df7jav21pe862", "igdfndgbifihqx", "4e8923df29872b86da2cd47295c7ac3b9b2b1b9a8d61029e6c47ad0eb792f7c3");
 $stmt = $pdo->prepare("INSERT INTO recipe_submissions (recipe_name, ingredients, steps, additional_notes) VALUES (:recipe_name, :ingredients, :steps, :additional_notes)");
 $stmt->execute([
     'recipe_name' => $recipeName,
